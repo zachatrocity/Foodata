b515a08c5cec43ddcd02e0872884ec541e16bd73
zrussell <zrussell1@SI-DSTEIL-1.harding.edu>
2015-04-22 13:41:04 -0500

    added the ability to arrow down and arrow up on the search results

--- Foodata/Foodata/Scripts/main.js
+++ Foodata/Foodata/Scripts/main.js
@@ -8,96 +8,133 @@ $(init);
 function init() {
     foodata.allfood = getAllFood();
 
-    $('#fuzzy-search').keyup(function () {
-        var search = $(this).val().toLowerCase();
+    $('#fuzzy-search').keyup(function (e) {
+        if (e.keyCode != 40 && e.keyCode != 38) {
+            var search = $(this).val().toLowerCase();
 
-        var rankcounter = 0;
-        /* Matching logic */
-        var matches = foodata.foodSearchArray.filter(function (food) {
-
-            var j = 0; // remembers position of last found character
-            rankcounter = 0;
-            food.matchindices = [];
-
-            if (food.foodname.toLowerCase().indexOf(search) != -1) {
-                if (food.foodname.toLowerCase().indexOf(search) == 0)
-                    rankcounter = 2000;//prioritize ones where the match is found at the beginning
-                else
-                    rankcounter = 1000;
-                for(var i = 0; i < search.length; i++)
-                {
-                    food.matchindices.push(food.foodname.toLowerCase().indexOf(search[i]));
-                }
+            if (search == "") {
+                //clear the list
+                $('#fuzzy-results').empty();
             }
             else {
-                // consider each search character one at a time 
-                for (var i = 0; i < search.length; i++) {
-                    var l = search[i];
-                    if (l == ' ') continue;     // ignore spaces
+                fuzzy(search);
+            }
+        }
 
-                    j = food.foodname.toLowerCase().indexOf(l);     // search for character & update position
-                    if (j == -1) {
-                        return false;  // if it's not found, exclude this item
-                    }
-                    else {
-                        food.matchindices.push(j); //else its found add its index to item
-                        for (var x = 0; x < food.matchindices.length; x++) {
-                            if (j >= food.matchindices[x]) {
-                                rankcounter += 5;
-                            }
-                            else
-                                rankcounter++;
-                        }
+    }).keyup();
 
-                    }
+
+    $('#fuzzy-search').keydown(function (e) {
+        if (e.keyCode == 40 || e.keyCode == 38) {
+            var $results = $('.fuzzy-item');
+            var $selected = $results.filter('.highlighted-item');
+            var $current;
+            var key = e.keyCode;
+
+            $results.removeClass('highlighted-item');
+
+            if (key == 40) // Down key
+            {
+                if (!$selected.length || $selected.is(':last-child')) {
+                    $current = $results.eq(0);
+                }
+                else {
+                    $current = $selected.next();
+                }
+            }
+            else if (key == 38) // Up key
+            {
+                if (!$selected.length || $selected.is(':first-child')) {
+                    $current = $results.last();
+                }
+                else {
+                    $current = $selected.prev();
                 }
             }
 
+            $current.addClass('highlighted-item');
+        }
+        else if (e.keyCode == 13) {
+            $res = $('.highlighted-item');
+            $res.children()[0].click()
+        }
 
-            //food.rank = food.foodname.length / rankcounter;
-            food.rank = rankcounter;
-            return true;
-        });
-        /* End matching logic */
+    });
 
-        //displaying logic
-        $('#fuzzy-results').empty();
-        var i = 0;
+}
 
-        //matches.sort(function (a, b) {
-        //    if (a.rank > b.rank)
-        //        return 1;
-        //    if (a.rank < b.rank)
-        //        return -1;
+function fuzzy(search) {
+    var rankcounter = 0;
+    /* Matching logic */
+    var matches = foodata.foodSearchArray.filter(function (food) {
 
-        //    return 0;
-        //});
+        var j = 0; // remembers position of last found character
+        rankcounter = 0;
+        food.matchindices = [];
 
-        //bubble sort to sort matches by rank
-        for(var a = 0; a < matches.length; a++)
-        {
-            for(var b = a; b < matches.length; b++)
-            {
-                if(matches[b].rank > matches[a].rank)
-                {
-                    var helper = matches[a];
-                    matches[a] = matches[b];
-                    matches[b] = helper;
+        if (food.foodname.toLowerCase().indexOf(search) != -1) {
+            if (food.foodname.toLowerCase().indexOf(search) == 0)
+                rankcounter = 2000;//prioritize ones where the match is found at the beginning
+            else
+                rankcounter = 1000;
+            for (var i = 0; i < search.length; i++) {
+                food.matchindices.push(food.foodname.toLowerCase().indexOf(search[i]));
+            }
+        }
+        else {
+            // consider each search character one at a time 
+            for (var i = 0; i < search.length; i++) {
+                var l = search[i];
+                if (l == ' ') continue;     // ignore spaces
+
+                j = food.foodname.toLowerCase().indexOf(l);     // search for character & update position
+                if (j == -1) {
+                    return false;  // if it's not found, exclude this item
+                }
+                else {
+                    food.matchindices.push(j); //else its found add its index to item
+                    for (var x = 0; x < food.matchindices.length; x++) {
+                        if (j >= food.matchindices[x]) {
+                            rankcounter += 5;
+                        }
+                        else
+                            rankcounter++;
+                    }
+
                 }
             }
         }
 
-        matches.forEach(function (match) {
-            if (i < 15) {
-                $('#fuzzy-results').append($('<li>').html(wrapFuzzyResultInBoldText(match)));
-                i++;
+        food.rank = rankcounter;
+        return true;
+    });
+    /* End matching logic */
+
+    //displaying logic
+    $('#fuzzy-results').empty();
+    var i = 0;
+
+    //bubble sort to sort matches by rank
+    for (var a = 0; a < matches.length; a++) {
+        for (var b = a; b < matches.length; b++) {
+            if (matches[b].rank > matches[a].rank) {
+                var helper = matches[a];
+                matches[a] = matches[b];
+                matches[b] = helper;
             }
-            else
-                return 0;
-        });
+        }
+    }
 
-    }).keyup();
-
+    matches.forEach(function (match) {
+        if (i < 15) {
+            $('#fuzzy-results').append($("<li class='fuzzy-item'>")
+                               .append($("<a class= 'fuzzylink' href='/api/food/" + match.primarykey + "'>")
+                               .append($("<span>").html(wrapFuzzyResultInBoldText(match)))));
+            i++;
+        }
+        else
+            return 0;
+    });
 }
 
 function wrapFuzzyResultInBoldText(match) {
