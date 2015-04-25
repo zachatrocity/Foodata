c3a1a8f01944e06752116f6588f5703919a5ae51
bmcfarland <bmcfarland@harding.edu>
2015-04-25 02:45:32 -0500

    Added auto filling values in update form
    Given the food guid in the query string in the form
    "/Home/Update?foodGuid=EnterNumbersHere" able to auto fill form

--- Foodata/Foodata/App_Start/BundleConfig.cs
+++ Foodata/Foodata/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace Foodata
             bundles.Add(new ScriptBundle("~/bundles/main").Include(
                         "~/Scripts/main.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/purl").Include(
+                        "~/Scripts/purl.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
